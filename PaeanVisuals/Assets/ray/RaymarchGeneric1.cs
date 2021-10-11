261db5d5143482f950afea7ad363c056cc4a64cf
Louis Cortes <cortes.lou@gmail.com>
2021-10-11 16:09:39 +0200

    ajout raymarching

---
+++ PaeanVisuals/Assets/ray/RaymarchGeneric1.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("Effects/Raymarch (Generic Complete1)")]
+public class RaymarchGeneric1 : MonoBehaviour
+{
+    public GameObject SunLight;
+    public GameObject center;
+    [SerializeField]
+    private Shader _EffectShader;
+    
+    [SerializeField]
+    private float _RaymarchDrawDistance = 40;
+
+    public Texture2D normal;
+    public Texture2D ao;
+    public Texture2D height;
+    public Texture2D rough;
+    public Texture2D albedo;
+
+    public Material EffectMaterial
+    {
+        get
+        {
+            if (!_EffectMaterial && _EffectShader)
+            {
+                _EffectMaterial = new Material(_EffectShader);
+                _EffectMaterial.hideFlags = HideFlags.HideAndDontSave;
+            }
+
+            return _EffectMaterial;
+        }
+    }
+    private Material _EffectMaterial;
+
+    public Camera CurrentCamera
+    {
+        get
+        {
+            if (!_CurrentCamera)
+                _CurrentCamera = GetComponent<Camera>();
+            return _CurrentCamera;
+        }
+    }
+    private Camera _CurrentCamera;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Matrix4x4 corners = GetFrustumCorners(CurrentCamera);
+        Vector3 pos = CurrentCamera.transform.position;
+
+        for (int x = 0; x < 4; x++) {
+            corners.SetRow(x, CurrentCamera.cameraToWorldMatrix * corners.GetRow(x));
+            Gizmos.DrawLine(pos, pos + (Vector3)(corners.GetRow(x)));
+        }
+
+    }
+
+    [ImageEffectOpaque]
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!EffectMaterial)
+        {
+            Graphics.Blit(source, destination); 
+            return;
+        }
+
+        EffectMaterial.SetVector("_LightDir", SunLight.transform.position);
+        EffectMaterial.SetVector("_wp", center.transform.position);
+        EffectMaterial.SetVector("_wr", center.transform.forward );
+        EffectMaterial.SetFloat("_DrawDistance", _RaymarchDrawDistance);
+        EffectMaterial.SetMatrix("_FrustumCornersES", GetFrustumCorners(CurrentCamera));
+        EffectMaterial.SetMatrix("_CameraInvViewMatrix", CurrentCamera.cameraToWorldMatrix);
+        EffectMaterial.SetVector("_CameraWS", CurrentCamera.transform.position);
+        EffectMaterial.SetTexture("_normal", normal);
+        EffectMaterial.SetTexture("_ao", ao);
+        EffectMaterial.SetTexture("_height", height);
+        EffectMaterial.SetTexture("_rough", rough);
+        EffectMaterial.SetTexture("_albedo", albedo);
+
+        CustomGraphicsBlit(source, destination, EffectMaterial, 0);
+    }
+    private Matrix4x4 GetFrustumCorners(Camera cam)
+    {
+        float camFov = cam.fieldOfView;
+        float camAspect = cam.aspect;
+
+        Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+        float fovWHalf = camFov * 0.5f;
+
+        float tan_fov = Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+
+        Vector3 toRight = Vector3.right * tan_fov * camAspect;
+        Vector3 toTop = Vector3.up * tan_fov;
+
+        Vector3 topLeft = (-Vector3.forward - toRight + toTop);
+        Vector3 topRight = (-Vector3.forward + toRight + toTop);
+        Vector3 bottomRight = (-Vector3.forward + toRight - toTop);
+        Vector3 bottomLeft = (-Vector3.forward - toRight - toTop);
+
+        frustumCorners.SetRow(0, topLeft);
+        frustumCorners.SetRow(1, topRight);
+        frustumCorners.SetRow(2, bottomRight);
+        frustumCorners.SetRow(3, bottomLeft);
+
+        return frustumCorners;
+    }
+
+    static void CustomGraphicsBlit(RenderTexture source, RenderTexture dest, Material fxMaterial, int passNr)
+    {
+        RenderTexture.active = dest;
+
+        fxMaterial.SetTexture("_MainTex", source);
+
+        GL.PushMatrix();
+        GL.LoadOrtho(); // Note: z value of vertices don't make a difference because we are using ortho projection
+
+        fxMaterial.SetPass(passNr);
+
+        GL.Begin(GL.QUADS);
+
+        // Here, GL.MultitexCoord2(0, x, y) assigns the value (x, y) to the TEXCOORD0 slot in the shader.
+        // GL.Vertex3(x,y,z) queues up a vertex at position (x, y, z) to be drawn.  Note that we are storing
+        // our own custom frustum information in the z coordinate.
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f); // BL
+
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f); // BR
+
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f); // TR
+
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f); // TL
+        
+        GL.End();
+        GL.PopMatrix();
+    }
+
+}
