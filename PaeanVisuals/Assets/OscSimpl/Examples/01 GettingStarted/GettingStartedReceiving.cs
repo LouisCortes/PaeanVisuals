fc784d0979d76b42f31af520531c3e00176be954
Louis Cortes <cortes.lou@gmail.com>
2021-10-13 15:14:06 +0200

    sahder4

--- PaeanVisuals/Assets/OscSimpl/Examples/01 GettingStarted/GettingStartedReceiving.cs
+++ PaeanVisuals/Assets/OscSimpl/Examples/01 GettingStarted/GettingStartedReceiving.cs
@@ -7,8 +7,8 @@
 
 using UnityEngine;
 
-namespace OscSimpl.Examples
-{
+//namespace OscSimpl.Examples
+//{
 	public class GettingStartedReceiving : MonoBehaviour
 	{
 		[SerializeField] OscIn _oscIn;
@@ -16,12 +16,36 @@ namespace OscSimpl.Examples
 		public string address1 = "/f7/f1";
 		public string address2 = "/1/1";
         public string address3 = "/1/1";
+        public string aneutre = "/1/1";
+        public string aaudio1 = "/1/1";
+        public string arotation = "/1/1";
+        public string azoom = "/1/1";
+        public string ableu = "/1/1";
+        public string acamer = "/1/1";
+        public string aliquide = "/liquide";
+        public string aaudio2 = "/audio2";
+        public string alow = "/low";
+        public string amid = "/mid";
+        public string ahigh = "/high";
+        public string a2fac = "/2fac";
         float _incomingFloat;
         private int Nbr_portIn;
         public float address01 ;
         public float address02;
         public float address03;
-        void Start()
+        public float neutre;
+        public float bleu;
+        public float audio1;
+        public float rotationv;
+        public float zoom;
+        public float camer;
+    public float liquide;
+    public float audio2;
+    public float low;
+    public float mid;
+    public float high;
+    public float fac2;
+    void Start()
 		{
             Nbr_portIn = _oscIn.port;
 			// Ensure that we have a OscIn component and start receiving on port 7000.
@@ -40,9 +64,21 @@ namespace OscSimpl.Examples
             _oscIn.MapFloat(address1, Event1);
             _oscIn.MapFloat(address2, Event2);
             _oscIn.MapFloat(address3, Event3);
-            // 2) For messages with multiple arguments, route the message using the Map method.
-            //_oscIn.Map( address2, OnCusto );
-        }
+            _oscIn.MapFloat(aaudio1, Event4);
+            _oscIn.MapFloat(arotation, Event5);
+            _oscIn.MapFloat(azoom, Event6);
+            _oscIn.MapFloat(ableu, Event7);
+            _oscIn.MapFloat(aneutre, Event8);
+            _oscIn.MapFloat(acamer, Event9);
+            _oscIn.MapFloat(aliquide, Event10);
+            _oscIn.MapFloat(aaudio2, Event11);
+            _oscIn.MapFloat(alow, Event12);
+            _oscIn.MapFloat(amid, Event13);
+            _oscIn.MapFloat(ahigh, Event14);
+            _oscIn.MapFloat(a2fac, Event15);
+        // 2) For messages with multiple arguments, route the message using the Map method.
+        //_oscIn.Map( address2, OnCusto );
+    }
 
 
 		void OnDisable()
@@ -79,8 +115,55 @@ namespace OscSimpl.Examples
           //  Debug.Log("Received f7/f1" + value + "\n");
             address03 = value;
         }
-
-        void OnTest2( OscMessage message )
+        void Event4(float value)
+        {
+           audio1  = value;
+        }
+        void Event5(float value)
+        {
+           rotationv  = value;
+        }
+        void Event6(float value)
+        {
+           zoom  = value;
+        }
+        void Event7(float value)
+        {
+           bleu  = value;
+        }
+        void Event8(float value)
+        {
+           neutre  = value;
+        }
+        void Event9(float value)
+        {
+           camer  = value;
+        }
+    void Event10(float value)
+    {
+        liquide = value;
+    }
+    void Event11(float value)
+    {
+        audio2 = value;
+    }
+    void Event12(float value)
+    {
+        low = value;
+    }
+    void Event13(float value)
+    {
+        mid = value;
+    }
+    void Event14(float value)
+    {
+        high = value;
+    }
+    void Event15(float value)
+    {
+         fac2 = value;
+    }
+    void OnTest2( OscMessage message )
 		{
             // Get arguments from index 0, 1 and 2 safely.
             float TestCelia;
@@ -123,4 +206,4 @@ namespace OscSimpl.Examples
 			OscPool.Recycle( message );
 		}
 	}
-}
+//}
