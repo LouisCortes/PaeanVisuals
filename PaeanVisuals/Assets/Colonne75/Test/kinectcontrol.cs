05ef4877a719285338d24169c0e7f55c5c9e7d4e
Louis Cortes <cortes.lou@gmail.com>
2021-10-16 20:56:19 +0200

    kinect control

---
+++ PaeanVisuals/Assets/Colonne75/Test/kinectcontrol.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kinectcontrol : MonoBehaviour
+{
+    public GettingStartedReceiving osc;
+    public Material rock;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rock.SetFloat("_liquide", osc.liquide);
+        rock.SetFloat("_audio1", osc.low * osc.fac2 * osc.address01);
+    }
+}
